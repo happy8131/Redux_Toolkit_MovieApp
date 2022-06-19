23eb311b2d99b50b413ab9f8c33018d5ec1d5529
happy8131 <lion8131@naver.com>
2022-06-19 17:02:59 +0900

    [Add] Search Slider

--- src/component/MovieListing/MovieListing.js
+++ src/component/MovieListing/MovieListing.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Slider from "react-slick";
 import { useSelector } from "react-redux";
 import { getAllMovies, getAllShows } from "../../features/movies/movieSlice";
 import MovieCard from "../MovieCard/MovieCard";
 import "./MovieListing.scss";
+import { Settings } from "../../common/settings";
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
@@ -35,11 +37,13 @@ const MovieListing = () => {
     <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
-        <div className="movie-container">{renderMovies}</div>
+        <div className="movie-container">
+          <Slider {...Settings}>{renderMovies}</Slider>
+        </div>
       </div>
       <div className="show-list">
         <h2>Shows</h2>
-        <div className="movie-container">{renderShows}</div>
+        <Slider {...Settings}>{renderShows}</Slider>
       </div>
     </div>
   );
